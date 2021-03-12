0f1664ad9d8ae009ce3d9e3101f2bf7c7df96d8f
Anthony <anthonybisignaro@gmail.com>
2021-03-12 16:56:29 -0500

    added 1st button

--- gui.py
+++ gui.py
@@ -3,6 +3,9 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+def button_click():
+    print("clicked")
+
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
@@ -12,6 +15,10 @@ def window():
     label = QtWidgets.QLabel(win)
     label.setText("My First Label")
     label.move(50,50)
+
+    b1 = QtWidgets.QPushButton(win)
+    b1.setText("click me")
+    b1.clicked.connect(button_click)
     
     win.show()
     sys.exit(app.exec_())
