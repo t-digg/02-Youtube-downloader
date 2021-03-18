3158be2be7ad87dd049018ee12264dee895d8445
Anthony <anthonybisignaro@gmail.com>
2021-03-17 23:19:29 -0400

    left only the bare minimum to keep the code running off just a ui file

--- RapidGUIprototyper.py
+++ RapidGUIprototyper.py
@@ -6,39 +6,39 @@ ytd = youtube_dl.YoutubeDL()
 
 
 #take input from user to get URL
-def read_input():
-    url = window.lineEdit.text()
-    url = [url]
-    print(url)
+#def read_input():
+    #url = window.lineEdit.text()
+    #url = [url]
+    #print(url)
     
-    window.lineEdit.clear()
+    #window.lineEdit.clear()
 
     #change downloads designation folder
-    environment_variables = dict(os.environ)
-    homepath = environment_variables['HOMEPATH']
-    downloads_folder = ('c:%s\\downloads' % homepath)
-    os.chdir(downloads_folder)
+    #environment_variables = dict(os.environ)
+    #homepath = environment_variables['HOMEPATH']
+    #downloads_folder = ('c:%s\\downloads' % homepath)
+    #os.chdir(downloads_folder)
 
     
     
     #download video        
-    ytd.download(url)
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
-    info = ydl.extract_info(url[0], download=False)
-    title = info["title"]
+   #ytd.download(url)
+   #ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+    #info = ydl.extract_info(url[0], download=False)
+    #title = info["title"]
 
 
     #hide original button and text
-    window.pushButton.hide()
-    window.lineEdit.hide()
+    #window.pushButton.hide()
+    #window.lineEdit.hide()
 
 
 
 
 
 
-    window.label.setText("Finished downloading " + title) #change the label from "" to display finished
-    window.label.adjustSize()
+    #window.label.setText("Finished downloading " + title) #change the label from "" to display finished
+    #window.label.adjustSize()
     
     
 
